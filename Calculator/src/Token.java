f3d6ac71f3d31eb97af754d0520c09483de1db9c
rynian <zunkerrr@mx.lakeforest.edu>
2017-11-27 22:07:12 -0600

    Add files via upload

--- Calculator/src/Token.java
+++ Calculator/src/Token.java
@@ -1,4 +1,39 @@
 
 public class Token {
-
+	int type;
+	float number;
+	public Token(float input)
+	{
+		type = 4;
+		number = input;
+	}
+	
+	public Token(String operator)
+	{
+		
+		switch(operator)
+		{
+			case "(":
+				type = 1;
+				
+			case "*":
+				type = 2;
+				break;
+			case "/":
+				type = 2;
+				break;
+			case "+":
+				type = 3;
+				break;
+			case "-":
+				type = 3;
+				break;
+			case ")":
+				type = 4;
+				break;
+				
+		}
+		
+		
+	}
 }
